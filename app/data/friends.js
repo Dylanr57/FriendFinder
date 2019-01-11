edf441c2eab3ead753976cc08c99553369caf9a9
Dylanr57 <dylanr574423@gmail.com>
2019-01-10 20:28:17 -0800

    part way done

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+var friendsData = [
+    {
+    name: "Fluffy",
+    photo: "https://i.pinimg.com/originals/a7/f7/73/a7f773018836201fb5e6d1e9a24049b8.jpg",
+    scores: [
+        1,2,3,4,5,4,3,2,1,2
+        ]
+    }
+]
+
+module.exports = friendsData;
